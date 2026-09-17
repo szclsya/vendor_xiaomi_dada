adffd8c6e515da09710ceccc1297332e70f3118f
Leo Shen <i@szclsya.me>
2026-09-16 20:41:23 -0400

    fix fingerprint HAL loading on goodix ultrasonic sensors
    Change-Id: I8064a0753f16dbaf24b6bd55f11390100d3eec3d

--- dada-vendor.mk
+++ dada-vendor.mk
@@ -7,8 +7,9 @@ PRODUCT_SOONG_NAMESPACES += \
 
 # Fingerprint HAL modules
 PRODUCT_COPY_FILES += \
-    vendor/xiaomi/dada/proprietary/odm/lib64/hw/fingerprint.goodix_us.default.so:$(TARGET_COPY_OUT_ODM)/lib64/hw/fingerprint.goodix.so \
-    vendor/xiaomi/dada/proprietary/odm/lib64/hw/fingerprint.qcom_us.default.so:$(TARGET_COPY_OUT_ODM)/lib64/hw/fingerprint.qcom_us.so \
+    vendor/xiaomi/dada/proprietary/odm/lib64/hw/fingerprint.goodix_us.default.so:$(TARGET_COPY_OUT_ODM)/lib64/hw/fingerprint.goodix_us.default.so \
+    vendor/xiaomi/dada/proprietary/odm/lib64/hw/fingerprint.qcom_us.default.so:$(TARGET_COPY_OUT_ODM)/lib64/hw/fingerprint.qcom_us.default.so \
+    vendor/xiaomi/dada/proprietary/vendor/lib64/vendor.qti.hardware.perf2-V1-ndk.so:$(TARGET_COPY_OUT_ODM)/lib64/vendor.qti.hardware.perf2-V1-ndk.so \
     vendor/xiaomi/dada/proprietary/odm/lib64/libqc_hal.so:$(TARGET_COPY_OUT_ODM)/lib64/libqc_hal.so
 
 # Graphics allocator V1 (needed by camera HAL blobs)
